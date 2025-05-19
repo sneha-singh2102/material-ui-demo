834777e60146c4b77681ebae501e1b7448b5dbd0
Sneha Singh <sneha.singh2102@gmail.com>
2025-05-19 14:11:33 +0530

    Added MUI button with props, theme and onclick event

---
+++ src/pages/Hosting/Hosting.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Grid from '@mui/material/Grid';
+
+const Authentication = () => {
+
+    return (
+        <Grid item xs={8}>
+            This is hosting page.
+        </Grid>
+    )
+}
+
+export default Authentication
